9d7a48433204166851196aae1986a0dc8d8ac692
Adnan Ahmed <adnan.ahmed316@gmail.com>
2020-12-07 15:51:09 +0500

    Update auto-unsub.decorator.ts
    issue fixed. this was undefined in component ngOnDestroy method

--- src/app/decorators/auto-unsub.decorator.ts
+++ src/app/decorators/auto-unsub.decorator.ts
@@ -10,7 +10,7 @@ export function AutoUnsub() {
                     property.unsubscribe()
                 }
             }
-            orig.apply();
+            orig.apply(this);
         }
     }
 }
